1faba69711b52beafae5c6abab084be651a5ec11
NaNGe <985933374@qq.com>
2020-08-03 14:30:28 +0800

    修改文件夹布局

---
+++ designer/demo.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'demo.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(80, 160, 118, 3))
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 40, 121, 61))
+        self.pushButton.setMinimumSize(QtCore.QSize(0, 61))
+        self.pushButton.setObjectName("pushButton")
+        self.calendarWidget = QtWidgets.QCalendarWidget(self.centralwidget)
+        self.calendarWidget.setGeometry(QtCore.QRect(50, 230, 200, 137))
+        self.calendarWidget.setObjectName("calendarWidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 18))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
--- src/basic/First.py
+++ src/basic/First.py
@@ -12,7 +12,6 @@ if __name__ == '__main__':
     w.resize(300, 150)
     # 移动窗口
     w.move(300, 300)
-
     # 设置窗口的标题
     w.setWindowTitle('第一个基于PyQt5的桌面应用')
     # 显示窗口
@@ -20,5 +19,4 @@ if __name__ == '__main__':
 
     # 进入程序的主循环，并通过exit函数确保主循环安全结束
     sys.exit(app.exec_())
-
 
