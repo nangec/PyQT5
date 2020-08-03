0cf6666f42c454cf8d21d3331afebe1c0decd9dd
NaNGe <985933374@qq.com>
2020-08-03 15:13:47 +0800

    垂直布局

--- designer/runMainWinHorizontalLayout.py
+++ designer/runMainWinHorizontalLayout.py
@@ -1,16 +1,9 @@
-import sys
+# -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+# Form implementation generated from reading ui file 'runMainWinHorizontalLayout.py'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
 
-import MainWinHorizontalLayout
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    main_window = QMainWindow()
-
-    # 创建ui的实例
-    ui = MainWinHorizontalLayout.Ui_MainWindow()
-    ##  向主窗口上添加空间
-    ui.setupUi(main_window)
-    main_window.show()
-    sys.exit(app.exec())
